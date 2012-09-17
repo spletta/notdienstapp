5ef7fa320e1790f19481e619778f61f6d9e2af32
Remington <remi500@freenet.de>
2012-09-17 09:42:44 +0200

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NdtAppV6::Application.config.session_store :cookie_store, key: '_ndt_app_v6_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NdtAppV6::Application.config.session_store :active_record_store
