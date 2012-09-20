fd96ea7092f3e88e64bb3281a72b7d996972a629
Remington <public@remingtoncharles.com>
2012-09-20 11:44:00 +0200

    fixed gravatar bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 NdtAppV6::Application.routes.draw do
-  resources :pharmacies
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
@@ -13,6 +9,9 @@ NdtAppV6::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
+  resources :pharmacies
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
