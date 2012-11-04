5125d559adabdd2e49ff9204ba67d2af9e690160
Remington <public@remingtoncharles.com>
2012-11-04 21:40:24 +0100

    forcing www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   private
     
     def current_account
-      if request.subdomain.present?
+      if request.subdomain.present? && request.subdomain != 'www'
         @account ||= Account.find_by_subdomain!(request.subdomain)
         #!Account.reserved_subdomain?(request.subdomain)
       #Account.find_by_subdomain! request.subdomain
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_account
   
     def scope_current_account
-      if request.subdomain.present?
+      if request.subdomain.present? && request.subdomain != 'www'
         Account.current_id = current_account.id   
       end
       yield
--- config.ru
+++ config.ru
@@ -2,3 +2,5 @@
 
 require ::File.expand_path('../config/environment',  __FILE__)
 run NdtAppV6::Application
+
+$stdout.sync = true
