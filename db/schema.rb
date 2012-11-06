59e485a4782b1e859a62059255f4764687b84714
Remington <public@remingtoncharles.com>
2012-11-06 12:28:43 +0100

    added missing migration again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121029141809) do
+ActiveRecord::Schema.define(:version => 20121106112751) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -28,12 +28,9 @@ ActiveRecord::Schema.define(:version => 20121029141809) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.integer  "pharmacy_id"
-    t.integer  "website_id"
     t.string   "website_title"
     t.string   "website_note"
   end
-
-  add_index "emergency_groups", ["website_id"], :name => "index_emergency_groups_on_website_id"
 
   create_table "events", :force => true do |t|
     t.integer  "pharmacy_id"
