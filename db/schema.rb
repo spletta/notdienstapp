d348aa6dc21b43e86d6eec65406b254d369b551c
Remington <public@remingtoncharles.com>
2012-11-21 20:30:28 +0100

    removed AddColorToEvent migration, updated mobile version of events form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121121113136) do
+ActiveRecord::Schema.define(:version => 20121120185238) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(:version => 20121121113136) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "account_id"
-    t.string   "color"
   end
 
   add_index "events", ["account_id"], :name => "index_events_on_account_id"
