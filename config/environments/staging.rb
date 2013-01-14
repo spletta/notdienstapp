e24e3228089a090a554c730d45f32e6e7569623f
Remington <public@remingtoncharles.com>
2013-01-14 15:24:56 +0100

    turning on http auth with rack middleware

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,10 @@
 NdtAppV6::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
+      [u, p] == [ENV['MY_SITE_USERNAME'], ENV['MY_SITE_SECRET']]
+  end
+    
   # Code is not reloaded between requests
   config.cache_classes = true
 
