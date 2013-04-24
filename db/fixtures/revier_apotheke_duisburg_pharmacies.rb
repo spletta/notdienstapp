adc55d1210591c5947dab117790c74c958b041f4
Remington <public@remingtoncharles.com>
2013-04-24 12:26:08 +0200

    added revier apotheke

---
+++ db/fixtures/revier_apotheke_duisburg_pharmacies.rb
@@ -0,0 +1,8 @@
+# this was addd over the UI
+#    :id => 11,
+#    :name => "Revier Apotheke",
+#    :subdomain => "revier-apotheke"
+
+
+# Some important notes:
+# All the pharmacies have already been created along with thier groups, a scanned in version of the notdienst plan shows which pharmacies have which ID's
