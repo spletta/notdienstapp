3501a683702f11d459865f1ef8f83486fa4e0e8a
Remington Splettstoesser <public@remingtoncharles.com>
2014-11-24 20:01:03 +0100

    added cleanup script

---
+++ db/fixtures/cleanup_accounts.rb
@@ -0,0 +1,42 @@
+EmergencyGroup.unscoped.delete_all(:account_id => 2)
+Pharmacy.unscoped.delete_all(:account_id => 2)
+Event.unscoped.delete_all(:account_id => 2)
+User.unscoped.delete_all(:account_id => 2)
+Account.unscoped.delete_all(:id => 2)
+puts 'Removed Muster Apotheke'
+
+EmergencyGroup.unscoped.delete_all(:account_id => 3)
+Pharmacy.unscoped.delete_all(:account_id => 3)
+Event.unscoped.delete_all(:account_id => 3)
+User.unscoped.delete_all(:account_id => 3)
+Account.unscoped.delete_all(:id => 3)
+puts 'Removed Brunnen Apotheke'
+
+EmergencyGroup.unscoped.delete_all(:account_id => 12)
+Pharmacy.unscoped.delete_all(:account_id => 12)
+Event.unscoped.delete_all(:account_id => 12)
+User.unscoped.delete_all(:account_id => 12)
+Account.unscoped.delete_all(:id => 12)
+puts 'Removed Signaleast Apotheke'
+
+EmergencyGroup.unscoped.delete_all(:account_id => 13)
+Pharmacy.unscoped.delete_all(:account_id => 13)
+Event.unscoped.delete_all(:account_id => 13)
+User.unscoped.delete_all(:account_id => 13)
+Account.unscoped.delete_all(:id => 13)
+puts 'Removed Brühl Apotheke'
+
+EmergencyGroup.unscoped.delete_all(:account_id => 14)
+Pharmacy.unscoped.delete_all(:account_id => 14)
+Event.unscoped.delete_all(:account_id => 14)
+User.unscoped.delete_all(:account_id => 14)
+Account.unscoped.delete_all(:id => 14)
+puts 'Removed Schloss Apotheke'
+
+
+EmergencyGroup.unscoped.delete_all(:account_id => 16)
+Pharmacy.unscoped.delete_all(:account_id => 16)
+Event.unscoped.delete_all(:account_id => 16)
+User.unscoped.delete_all(:account_id => 16)
+Account.unscoped.delete_all(:id => 16)
+puts 'Removed aswwerdau.de'
